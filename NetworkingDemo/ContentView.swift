ec6082579df188d413f36bd961c4b7263b1f4c1e
tamizhselvangt <tamizhselvangt@gmail.com>
2024-12-08 16:23:08 +0530

    Initial Commit

---
+++ NetworkingDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkingDemo
+//
+//  Created by Tamizhselvan gurusamy on 12/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkingDemo/NetworkingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingDemoApp.swift
+//  NetworkingDemo
+//
+//  Created by Tamizhselvan gurusamy on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
